2b51d44d0853c675d86469e75422d40770ab4dbc
Mathias Buus <mathiasbuus@gmail.com>
2024-12-05 19:58:32 +0100

    add example

---
+++ example.mjs
@@ -0,0 +1,26 @@
+import Autopass from './index.js'
+import Corestore from 'corestore'
+
+const store = new Corestore('example/' + process.argv[2])
+
+let pass = null
+
+if (process.argv[3]) {
+  const pair = Autopass.pair(store, process.argv[3])
+  pass = await pair.finished()
+} else {
+  pass = new Autopass(store)
+}
+
+if (pass.base.writable) {
+  const inv = await pass.createInvite()
+  console.log('invite', inv)
+}
+
+onupdate()
+pass.on('update', onupdate)
+
+function onupdate () {
+  console.log('db changed, all entries:')
+  pass.list().on('data', console.log)
+}
